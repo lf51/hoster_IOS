30b96a3e539c0dd6f4e2bb1df0b0bc8a476e9739
Lillo <lillofriscia@gmail.com>
2024-02-28 15:12:02 +0100

    Initial Commit

---
+++ hoster/hosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  hosterApp.swift
+//  hoster
+//
+//  Created by Calogero Friscia on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct hosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
